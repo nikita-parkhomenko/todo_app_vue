34e58a18f5e5e44932cf74d48c26d59443b1c805
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-24 21:32:20 +0300

    created Todos and TodoItems components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        { title: 'Learn Vue js', id: 1, completed: false },
+        { title: 'Learn TypeScript', id: 2, completed: true },
+        { title: 'Develop Test Task', id: 3, completed: false },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
